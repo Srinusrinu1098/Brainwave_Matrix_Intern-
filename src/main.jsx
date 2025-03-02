2aaf9f00188cfe886328af1b1c41d1678f260283
Srinusrinu1098 <tsrinivasulu1098@gmail.com>
2025-03-02 21:55:20 +0530

    Changes made

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GoogleOAuthProvider
         clientId={import.meta.env.VITE_GOOGLE_LOGIN_CLEINT_ID}
       >
         <App />
       </GoogleOAuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
