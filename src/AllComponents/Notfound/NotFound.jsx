5b2e74b23cf637e91084f3df504c517d0bac8edd
Srinusrinu1098 <tsrinivasulu1098@gmail.com>
2025-03-03 22:33:26 +0530

    added Readme

--- src/AllComponents/Notfound/NotFound.jsx
+++ src/AllComponents/Notfound/NotFound.jsx
@@ -1,9 +1,24 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <img src="/404.png" alt="404 Not Found" className="w-64 mb-4" />
+      <h1 className="text-3xl font-bold text-gray-800 flex items-center gap-2">
+        <span className="animate-spin">🚫</span> Oops! Page Not Found
+      </h1>
+      <p className="text-gray-600 mt-2">
+        The page you’re looking for doesn’t exist or has been moved.
+      </p>
+      <Button
+        onClick={() => navigate("/")}
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg transition"
+      >
+        Go Home
+      </Button>
     </div>
   );
 }
